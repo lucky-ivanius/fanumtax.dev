8aa17b643d8c7ca096da5763f2102ca206a7df53
Lucky Ivanius <luckzivanius@gmail.com>
2025-10-13 03:56:59 +0700

    feat(app): Initialize next.js application

---
+++ apps/app/src/app/layout.tsx
@@ -0,0 +1,24 @@
+import type { Metadata } from "next";
+import { Geist_Mono } from "next/font/google";
+import "./globals.css";
+
+const geistMono = Geist_Mono({
+  variable: "--font-geist-mono",
+  subsets: ["latin"],
+});
+
+export const metadata: Metadata = {
+  title: "fanumtax.dev",
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en">
+      <body className={`dark ${geistMono.variable} antialiased`}>{children}</body>
+    </html>
+  );
+}
---
+++ apps/app/src/app/page.tsx
@@ -0,0 +1,3 @@
+export default function Dashboard() {
+  return <div className="bg-background font-bold text-7xl">BUILD</div>;
+}
