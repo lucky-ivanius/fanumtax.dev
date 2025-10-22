4aa2b04b04ffb98e046dc26848b5a8411a398b11
Lucky Ivanius <luckzivanius@gmail.com>
2025-10-23 06:29:14 +0700

    feat(app): enhance issue list and card components

--- apps/app/src/components/repo/issue-card.tsx
+++ apps/app/src/components/repo/issue-card.tsx
@@ -1,14 +1,17 @@
 import Link from "next/link";
 
 import type { Issue } from "@fanumtax/core/issue";
-import { Card, CardContent, CardDescription, CardHeader, CardTitle } from "@fanumtax/ui/components/card";
+import { Badge } from "@fanumtax/ui/components/badge";
+import { Card, CardContent, CardDescription, CardFooter, CardHeader, CardTitle } from "@fanumtax/ui/components/card";
 
 interface IssueCardProps {
+  owner: string;
+  repo: string;
   issue: Issue;
 }
 
-export const IssueCard: React.FC<IssueCardProps> = ({ issue }) => {
-  const { owner, repo, number, title, body, bounty } = issue;
+export const IssueCard: React.FC<IssueCardProps> = ({ owner, repo, issue }) => {
+  const { number, title, labels, createdAt } = issue;
 
   return (
     <Card className="relative h-full border shadow-none transition-all hover:border-primary active:border-primary">
@@ -22,9 +25,27 @@ export const IssueCard: React.FC<IssueCardProps> = ({ issue }) => {
             <span className="text-muted-foreground text-sm"> #{number}</span>
           </Link>
         </CardTitle>
-        <CardDescription>{body}</CardDescription>
+        <CardDescription>
+          <div className="flex items-center gap-1">
+            {labels.map(({ name, color }) => (
+              <Badge
+                key={name}
+                variant="outline"
+                style={{
+                  borderColor: color,
+                }}
+              >
+                {name}
+              </Badge>
+            ))}
+          </div>
+        </CardDescription>
       </CardHeader>
-      <CardContent></CardContent>
+      <CardFooter>
+        <div className="flex items-center gap-2">
+          <span className="text-muted-foreground text-sm">{new Date(createdAt).toLocaleString()}</span>
+        </div>
+      </CardFooter>
     </Card>
   );
 };
--- apps/app/src/components/repo/issue-list.tsx
+++ apps/app/src/components/repo/issue-list.tsx
@@ -1,17 +1,92 @@
+"use client";
+
+import { useEffect, useState, useTransition } from "react";
+
 import type { Issue } from "@fanumtax/core/issue";
+import { Button } from "@fanumtax/ui/components/button";
+
+import { searchIssues } from "@/lib/api";
 
 import { IssueCard } from "./issue-card";
 
 interface IssueListProps {
-  issues: Issue[];
+  owner: string;
+  repo: string;
+  initialIssues: Issue[];
+  findMoreOptions?: {
+    q?: string;
+  };
 }
 
-export const IssueList: React.FC<IssueListProps> = ({ issues }) => {
+const defaultLimit = 15;
+
+export const IssueList: React.FC<IssueListProps> = ({ owner, repo, initialIssues, findMoreOptions = {} }) => {
+  const [issues, setIssues] = useState(initialIssues);
+
+  const isSearch = !!Object.keys(findMoreOptions).length;
+
+  useEffect(() => {
+    setIssues(initialIssues);
+  }, [initialIssues]);
+
+  const [isLoading, startTransition] = useTransition();
+
+  const findMoreIssues = () => {
+    startTransition(async () => {
+      const searchIssuesResult = await searchIssues({
+        platform: "github",
+        owner,
+        repo,
+        ...findMoreOptions,
+        offset: issues.length,
+        limit: defaultLimit,
+      });
+      if (!searchIssuesResult.success)
+        switch (searchIssuesResult.error) {
+          case "unexpected_error":
+            throw new Error("Internal server error");
+        }
+
+      const { items } = searchIssuesResult.data;
+
+      setIssues((prevIssues) => [...prevIssues, ...items]);
+    });
+  };
+
   return (
-    <div className="grid auto-cols-fr grid-cols-1 gap-4">
-      {issues.map((issue) => (
-        <IssueCard key={issue.number} issue={issue} />
-      ))}
+    <div className="flex flex-col items-center justify-center gap-6">
+      {issues.length ? (
+        <div className="grid w-full auto-cols-fr grid-cols-1 gap-4">
+          {issues.map((issue) => (
+            <IssueCard key={issue.number} owner={owner} repo={repo} issue={issue} />
+          ))}
+        </div>
+      ) : (
+        <div className="flex w-full flex-col items-center justify-center gap-4 py-12">
+          <h2 className="text-center text-3xl text-foreground">{isSearch ? "No issues found" : "All Clear!"}</h2>
+          {isSearch ? (
+            <p className="text-center text-muted-foreground text-sm">
+              We couldn't find any issues that match your search criteria.
+            </p>
+          ) : (
+            <>
+              <p className="text-center text-muted-foreground text-sm">
+                Have an issue to report? Open one and set a bounty to attract contributors!
+              </p>
+              <Button asChild>
+                <a href={`https://github.com/${owner}/${repo}/issues/new`} target="_blank" rel="noopener noreferrer">
+                  Open New Issue
+                </a>
+              </Button>
+            </>
+          )}
+        </div>
+      )}
+      {!(issues.length < defaultLimit || issues.length >= 1000 - defaultLimit) && (
+        <Button variant="secondary" onClick={findMoreIssues} disabled={isLoading}>
+          {isLoading ? "Loading..." : "Load more"}
+        </Button>
+      )}
     </div>
   );
 };
