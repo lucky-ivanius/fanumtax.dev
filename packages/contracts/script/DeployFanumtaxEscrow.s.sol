39992dfac212c18ab8d14fff5f72bfe699ea874e
Lucky Ivanius <luckzivanius@gmail.com>
2025-10-25 03:49:09 +0700

    chore(contracts): update solidity compiler version to 0.8.30

--- packages/contracts/script/DeployFanumtaxEscrow.s.sol
+++ packages/contracts/script/DeployFanumtaxEscrow.s.sol
@@ -1,5 +1,5 @@
 // SPDX-License-Identifier: MIT
-pragma solidity ^0.8.28;
+pragma solidity ^0.8.30;
 
 import {Script, console} from "forge-std/Script.sol";
 import {FanumtaxEscrow} from "../src/FanumtaxEscrow.sol";
